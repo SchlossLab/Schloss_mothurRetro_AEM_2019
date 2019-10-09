4bb8f416f2eb41e0f1c54eca797a291488e32456
Pat Schloss <pschloss@umich.edu>
2019-10-09 15:51:23 -0400

    Incorporate MB / KS comments

--- code/plot_citations.R
+++ code/plot_citations.R
@@ -1,10 +1,19 @@
 library(tidyverse)
 
-read_tsv('data/citations.tsv') %>%
+actual <- read_tsv('data/citations.tsv') %>%
 	gather(-year, key="tool", value="citations") %>%
 	filter(tool == "mothur") %>%
-	ggplot(aes(x=year, y=citations)) +
-		geom_line() +
+	select(-tool) %>%
+	mutate(numbers = "actual")
+
+projected <- actual %>%
+	mutate(numbers = "projected") %>%
+	mutate(citations = if_else(year == 2019, citations * 3/2, citations))
+
+rbind(projected, actual)	%>%
+	ggplot(aes(x=year, y=citations, color=reorder(numbers, desc(numbers)))) +
+		geom_line(show.legend=FALSE) +
+		scale_color_manual(values=c("gray", "black")) +
 		theme_classic() +
 		labs(x="Year", y="Number of Citations") +
 		scale_x_continuous(breaks=seq(2009,2019,2), labels=seq(2009,2019,2))
